ed54712819f0d1129f48929b52ad7c4fb21ac3cd
brickatyourfeet <brickatyourfeet@gmail.com>
2019-12-03 19:39:14 -0800

    add new view controller file

---
+++ Definitions/FactsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FactsViewController.swift
+//  Definitions
+//
+//  Created by Kyle Braden on 12/3/19.
+//  Copyright © 2019 Kyle Braden. All rights reserved.
+//
+
+import UIKit
+
+class FactsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
