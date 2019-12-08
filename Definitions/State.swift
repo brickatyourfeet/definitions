784420dc2fca205e73302127a048e65cf468c7cd
brickatyourfeet <brickatyourfeet@gmail.com>
2019-12-08 13:43:23 -0800

    add state class

---
+++ Definitions/State.swift
@@ -0,0 +1,17 @@
+//
+//  State.swift
+//  Definitions
+//
+//  Created by Kyle Braden on 12/8/19.
+//  Copyright © 2019 Kyle Braden. All rights reserved.
+//
+
+import Foundation
+
+class State {
+    var name = ""
+    
+    //struct for facts?
+    var facts = "facts"
+    
+}
